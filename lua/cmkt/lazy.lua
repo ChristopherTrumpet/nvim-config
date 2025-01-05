082f8979a001b7c721944a00255c822d0bd9f7dc
Christopher Trumpet <chris@Mac.home>
2025-01-05 00:19:52 -0500

    additional plugins

--- lua/cmkt/lazy.lua
+++ lua/cmkt/lazy.lua
@@ -11,7 +11,11 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("cmkt.plugins", {
+require("lazy").setup({ { import = "cmkt.plugins" }, { import = "cmkt.plugins.lsp" } }, {
+  checker = {
+    enabled = true,
+    notify = false,
+  },
   change_detection = {
     notify = false,
   },
